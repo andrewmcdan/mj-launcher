5d802d61d92a811517b69d839b5c68756908b188
Andrew <andrewmcdan@gmail.com>
2023-12-02 19:26:06 -0500

    add upadte otions

--- launcher.js
+++ launcher.js
@@ -9,22 +9,27 @@ env.DISPLAY = ':0';
 class Launcher {
     constructor() {
         this.chromiumPath = '/usr/bin/chromium-browser';
-        this.chromiumOptions = [
-            '--noerrdialogs',
-            '--disable-infobars',
-            '--kiosk',
-            'http://mj-downloader.lan:3001/show?enableAutoAdjustUpdateInterval=false&updateInterval=11&fadeDuration=3.5'
-        ];
+
         this.env = Object.create(process.env);
         this.env.DISPLAY = ':0';
         this.running = false;
         this.timeToRestart = 0;
         this.runTimeout = null;
         this.chromium = null;
-        this.run();
+        this.startRestart();
+        this.optionsUptdate = null;
+        this.enableAutoAdjustUpdateInterval = false;
+        this.updateInterval = 11;
+        this.fadeDuration = 3.5;
+        this.chromiumOptions = [
+            '--noerrdialogs',
+            '--disable-infobars',
+            '--kiosk',
+            `http://mj-downloader.lan:3001/show?enableAutoAdjustUpdateInterval=${this.enableAutoAdjustUpdateInterval ? 'true' : 'false'}&updateInterval=${this.updateInterval}&fadeDuration=${this.fadeDuration}`
+        ];
     }
 
-    start() {
+    run() {
         if (this.runTimeout !== null) {
             clearTimeout(this.runTimeout);
         }
@@ -35,11 +40,15 @@ class Launcher {
             timeUntilRestart += 1000 * 60 * 60 * 24;
         }
         this.runTimeout = setTimeout(() => {
-            this.run();
+            this.startRestart();
         }, timeUntilRestart);
+
+        this.optionsUptdate = setInterval(() => {
+            this.updateOptions();
+        }, 1000 * 60);
     }
 
-    run() {
+    startRestart() {
         if (this.running) {
             // kill chromium
             this.chromium.kill();
@@ -56,9 +65,14 @@ class Launcher {
             console.log(`Chromium process exited with code ${code}`);
             this.running = false;
         });
-        this.start();
+        this.run();
+    }
+
+    async updateOptions() {
+        let options = await fetch('http://mj-downloader.lan:3001/showOptions').then(res => res.json());
+        console.log(options);
     }
 };
 
 let launcher = new Launcher();
-launcher.start();
+launcher.run();
