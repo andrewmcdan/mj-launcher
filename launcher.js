52493381c47005c205475159fce510e51b352866
Andrew <andrewmcdan@gmail.com>
2023-12-13 16:47:11 -0500

    ...

--- launcher.js
+++ launcher.js
@@ -8,7 +8,7 @@ const env = Object.create(process.env);
 env.DISPLAY = ':0';
 
 const resolutions = [
-    { width: 4096, height: 2161 },
+    { width: 4096, height: 2160 },
     { width: 3840, height: 2160 },
     { width: 1920, height: 1080 },
     { width: 1280, height: 720 },
